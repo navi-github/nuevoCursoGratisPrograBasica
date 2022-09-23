32da222867ea13bf415b52b76108f61f9a221842
Ivan <hp_692c@hotmail.com>
2022-09-23 06:37:02 -0500

    Separamos los archivos .html y .js

---
+++ mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador() {
+    alert("Seleccionaste tu mascota")
+}
+
+let botonMascotaJugador = document.getElementById("boton-mascota");
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
