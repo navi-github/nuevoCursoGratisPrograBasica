237666951a59d90c35d46012c405074e5ca930c9
Ivan <hp_692c@hotmail.com>
2022-10-09 12:10:59 -0500

    Agregamos función inner.HTML para mostrar la mascota seleccionada

--- mokepon.js
+++ mokepon.js
@@ -8,14 +8,19 @@ function seleccionarMascotaJugador() {
     const inputCapipego = document.getElementById('Capipego');
     const inputRatigueya = document.getElementById('Ratigueya');
 
+    const mascotaJugador = document.getElementById('mascotaJugador')
+
     if(inputHipodoge.checked) {
-        alert("Seleccionaste a Hipodoge");
+        // alert("Seleccionaste a Hipodoge");
+        mascotaJugador.innerHTML = "Hipodoge";
     } 
     else if(inputCapipego.checked) {
-        alert("Seleccionaste a Capipego");
+        // alert("Seleccionaste a Capipego");
+        mascotaJugador.innerHTML = "Capipego";
     }
     else if(inputRatigueya.checked) {
-        alert("Seleccionaste a Ratigueya");
+        // alert("Seleccionaste a Ratigueya");
+        mascotaJugador.innerHTML = "Ratigueya";
     } else {
         alert("Por favor selecciona tu mascota");
     }
