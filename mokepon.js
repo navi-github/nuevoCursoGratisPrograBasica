b29753bc0ec040ac150617be4737486295c76036
Ivan <hp_692c@hotmail.com>
2022-10-18 07:41:17 -0500

    Agregamos lógica de ataques aleatorios enemigo

--- mokepon.js
+++ mokepon.js
@@ -1,4 +1,5 @@
 let ataqueJugador;
+let ataqueEnemigo;
 
 function iniciarJuego() { 
     let botonMascotaJugador = document.getElementById("boton-mascota");
@@ -42,12 +43,12 @@ function aleatorio(min, max) {
 }
 
 function seleccionaMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1,3);
+    let mascotaAleatoriaEnemigo = aleatorio(1,3);
     const mascotaEnemigo = document.getElementById('mascotaEnemigo');
 
-    if (ataqueAleatorio == 1) { 
+    if (mascotaAleatoriaEnemigo == 1) { 
         mascotaEnemigo.innerHTML = "Hipodoge";
-    } else if (ataqueAleatorio == 2) {
+    } else if (mascotaAleatoriaEnemigo == 2) {
         mascotaEnemigo.innerHTML = "Capipego";
     } else {
         mascotaEnemigo.innerHTML = "Ratigueya";
@@ -58,20 +59,34 @@ function ataqueFuego() {
     ataqueJugador = 'FUEGO';
     let spanAtaqueJugador = document.getElementById('ataque-jugador');
     spanAtaqueJugador.innerHTML = ataqueJugador;
+    ataqueAleatorioEnemigo();
 }
 
 function ataqueAgua() {
     ataqueJugador = 'AGUA';
     let spanAtaqueJugador = document.getElementById('ataque-jugador');
     spanAtaqueJugador.innerHTML = ataqueJugador;
+    ataqueAleatorioEnemigo();
 }
 
 function ataqueTierra() {
     ataqueJugador = 'TIERRA';
     let spanAtaqueJugador = document.getElementById('ataque-jugador');
     spanAtaqueJugador.innerHTML = ataqueJugador;
+    ataqueAleatorioEnemigo();
 }
 
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3);
+
+    if(ataqueAleatorio == 1) {
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2){
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }
+}
 
 
 window.addEventListener('load',  iniciarJuego)
